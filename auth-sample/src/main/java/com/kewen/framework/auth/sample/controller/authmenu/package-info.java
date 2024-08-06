15adbefb5c6a314f4891e37611b1e265ee69ceef
kewen <liukewensc@outlook.com>
2024-08-06 23:28:34 +0800

    remove: 去除sample示例工程,因开源协议限制，不能有mysql驱动，因此示例工程单独提取至kewen-framwork-auth-sample中

--- auth-sample/src/main/java/com/kewen/framework/auth/sample/controller/authmenu/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.kewen.framework.auth.sample.controller.authmenu;
-
-/**
- * 主要是AuthMenu生成时相关的，
- * 验证{@link com.kewen.framework.boot.auth.init.InitMenuAuthCommandLineRunner}
- * 对于API菜单加载的正确性
- */
